eeaae63665266821f03309a0bd4395136026c153
dwb1210 <104562052+dwb1210@users.noreply.github.com>
2022-06-06 14:37:46 +0100

    Countdown Upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var deadline = new Date("jun 11, 2022 07:00:00").getTime();
+
+var x = setInterval(function() {
+
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("day").innerHTML =days ;
+document.getElementById("hour").innerHTML =hours;
+document.getElementById("minute").innerHTML = minutes;
+document.getElementById("second").innerHTML =seconds;
+if (t < 0) {
+		clearInterval(x);
+		document.getElementById("clockdiv").style.display = "none";
+		document.getElementById("day").style.display = "none";
+		document.getElementById("hour").style.display = "none";
+		document.getElementById("minute").style.display = "none";
+		document.getElementById("second").style.display = "none";
+		document.getElementById("text").style.display = "block";
+		document.getElementById("pic").style.display = "block";
+		document.body.style.background = "linear-gradient(to bottom right, #202A44 , #6495ED";
+		}
+}, 1000);
